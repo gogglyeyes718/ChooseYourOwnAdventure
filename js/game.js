ddea798725e6b7d36de3d1cd9270f0cb2c21680f
Taifinkaba <taifinkabad@gmail.com>
2018-03-19 22:34:11 +0000

    this is your game

--- js/game.js
+++ js/game.js
@@ -6,24 +6,42 @@ var game = {
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            message: "You come across a 4 man Squad. What do you do?",
             choices: [
                 {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
+                    text: "Attack",
+                    nextLevel: "Thebattle",
                 },
 
                 {
-                    text: "Keep on moving",
-                    nextLevel: "field",
+                    text: "Hide",
+                    nextLevel: "HideWhere",
                 },
             ]
         },
 
-        cave: {
+        Thebattle: {
             background_image: "fire.gif",
             music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+            message: "You see a 4 man Squad, choose your weapon.",
+            choices: [
+                {
+                    text: "AR",
+                    nextLevel: "fight",
+                },
+                {
+                    text: "Shotgun",
+                    nextLevel: "fight",
+                },
+                {
+                    text: "SMG",
+                    nextLevel: "fight",
+                },
+            ]
+        },
+
+        fight: {
+            message: "You managed to knock two of the four enemy but the other remaining two knocked you and then finished you.",
             choices: [
                 {
                     text: "Start over",
@@ -31,16 +49,68 @@ var game = {
                 },
             ]
         },
-
-        field: {
-            message: "Some adventurer you are...",
+        
+        HideWhere: {
+            message: "You see two things that can hide you. What do you choose?",
             choices: [
                 {
-                    text: "Start over",
+                    text: "Bush",
+                    nextLevel: "Bush",
+                },
+                 {
+                    text: "Tree",
+                    nextLevel: "intree",
+                },
+            ]
+        },
+        Bush: {
+            message: "One of the enemies player tripped on you and they all shot at you, knocked you, and danced on you till you bled out.",
+            choices: [
+                {
+                    text: "Congrats, You played Yourself!",
+                    nextLevel: "start",
+                },
+            ]
+        },    
+            intree: {
+            message: "They left, now what will you do?",
+            choices: [
+                {
+                    text: "Suprise attack",
+                    nextLevel: "Suprise",
+                },
+                {
+                    text: "Tell team",
+                    nextLevel: "Run",
+                },
+            ]
+        },
+        Suprise: {
+            message: "You managed to knock two of the four enemy but the other remaining two knocked you and then finished you.",
+            choices: [
+                {
+                    text: "Congrats, You played Yourself!",
                     nextLevel: "start",
                 },
             ]
         },
-
+        Run: {
+            message: "You managed to tell your team and one of your player said that your squad will fight'em.",
+            choices: [
+                {
+                    text: "let's do this",
+                    nextLevel: "fourvfour",
+                },
+            ]
+        },
+        fourvfour: {
+            message: "You had a great battle but the enemy killed you and two of your comrades. The last player on your team was the one who recomended the fight but left you guys right from the beggining of the fight, meaning you did a 3v4.",
+            choices: [
+                {
+                    text: "Congrats, You played Yourself! Shows you for playing with Jayden.",
+                    nextLevel: "start",
+                },
+            ]
+        },
     }
 };
